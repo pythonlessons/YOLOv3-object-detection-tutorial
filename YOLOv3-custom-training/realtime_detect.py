7b0faeee6dbc73b79b0c58cdcb87f840c411b4cf
pythonlessons <43852392+pythonlessons@users.noreply.github.com>
2019-08-25 21:53:32 +0300

    Add files via upload

---
+++ YOLOv3-custom-training/realtime_detect.py
@@ -0,0 +1,220 @@
+import colorsys
+import os
+os.environ['CUDA_VISIBLE_DEVICES'] = '2'
+import cv2
+
+import numpy as np
+from keras import backend as K
+from keras.models import load_model
+from keras.layers import Input
+
+from yolo3.model import yolo_eval, yolo_body, tiny_yolo_body
+from yolo3.utils import image_preporcess
+
+import multiprocessing
+from multiprocessing import Pipe
+import mss
+import time
+
+# set start time to current time
+start_time = time.time()
+# displays the frame rate every 2 second
+display_time = 2
+# Set primarry FPS to 0
+fps = 0
+
+start_time = time.time()
+display_time = 2 # displays the frame rate every 2 second
+fps = 0
+sct = mss.mss()
+# Set monitor size to capture
+monitor = {"top": 40, "left": 0, "width": 1080, "height": 1080}
+
+
+class YOLO(object):
+    _defaults = {
+        #"model_path": 'logs/ep050-loss21.173-val_loss19.575.h5',
+        "model_path": 'logs/trained_weights_final.h5',
+        "anchors_path": 'model_data/yolo_anchors.txt',
+        "classes_path": '4_CLASS_test_classes.txt',
+        "score" : 0.3,
+        "iou" : 0.45,
+        "model_image_size" : (416, 416),
+        "text_size" : 3,
+    }
+
+    @classmethod
+    def get_defaults(cls, n):
+        if n in cls._defaults:
+            return cls._defaults[n]
+        else:
+            return "Unrecognized attribute name '" + n + "'"
+
+    def __init__(self, **kwargs):
+        self.__dict__.update(self._defaults) # set up default values
+        self.__dict__.update(kwargs) # and update with user overrides
+        self.class_names = self._get_class()
+        self.anchors = self._get_anchors()
+        self.sess = K.get_session()
+        self.boxes, self.scores, self.classes = self.generate()
+
+    def _get_class(self):
+        classes_path = os.path.expanduser(self.classes_path)
+        with open(classes_path) as f:
+            class_names = f.readlines()
+        class_names = [c.strip() for c in class_names]
+        return class_names
+
+    def _get_anchors(self):
+        anchors_path = os.path.expanduser(self.anchors_path)
+        with open(anchors_path) as f:
+            anchors = f.readline()
+        anchors = [float(x) for x in anchors.split(',')]
+        return np.array(anchors).reshape(-1, 2)
+
+    def generate(self):
+        model_path = os.path.expanduser(self.model_path)
+        assert model_path.endswith('.h5'), 'Keras model or weights must be a .h5 file.'
+
+        # Load model, or construct model and load weights.
+        num_anchors = len(self.anchors)
+        num_classes = len(self.class_names)
+        is_tiny_version = num_anchors==6 # default setting
+        try:
+            self.yolo_model = load_model(model_path, compile=False)
+        except:
+            self.yolo_model = tiny_yolo_body(Input(shape=(None,None,3)), num_anchors//2, num_classes) \
+                if is_tiny_version else yolo_body(Input(shape=(None,None,3)), num_anchors//3, num_classes)
+            self.yolo_model.load_weights(self.model_path) # make sure model, anchors and classes match
+        else:
+            assert self.yolo_model.layers[-1].output_shape[-1] == \
+                num_anchors/len(self.yolo_model.output) * (num_classes + 5), \
+                'Mismatch between model and given anchor and class sizes'
+
+        print('{} model, anchors, and classes loaded.'.format(model_path))
+
+        # Generate colors for drawing bounding boxes.
+        hsv_tuples = [(x / len(self.class_names), 1., 1.)
+                      for x in range(len(self.class_names))]
+        self.colors = list(map(lambda x: colorsys.hsv_to_rgb(*x), hsv_tuples))
+        self.colors = list(
+            map(lambda x: (int(x[0] * 255), int(x[1] * 255), int(x[2] * 255)),
+                self.colors))
+
+        np.random.shuffle(self.colors)  # Shuffle colors to decorrelate adjacent classes.
+
+        # Generate output tensor targets for filtered bounding boxes.
+        self.input_image_shape = K.placeholder(shape=(2, ))
+        boxes, scores, classes = yolo_eval(self.yolo_model.output, self.anchors,
+                len(self.class_names), self.input_image_shape,
+                score_threshold=self.score, iou_threshold=self.iou)
+        return boxes, scores, classes
+
+    def detect_image(self, image):
+        if self.model_image_size != (None, None):
+            assert self.model_image_size[0]%32 == 0, 'Multiples of 32 required'
+            assert self.model_image_size[1]%32 == 0, 'Multiples of 32 required'
+            boxed_image = image_preporcess(np.copy(image), tuple(reversed(self.model_image_size)))
+            image_data = boxed_image
+
+        out_boxes, out_scores, out_classes = self.sess.run(
+            [self.boxes, self.scores, self.classes],
+            feed_dict={
+                self.yolo_model.input: image_data,
+                self.input_image_shape: [image.shape[0], image.shape[1]],#[image.size[1], image.size[0]],
+                K.learning_phase(): 0
+            })
+
+        #print('Found {} boxes for {}'.format(len(out_boxes), 'img'))
+
+        thickness = (image.shape[0] + image.shape[1]) // 600
+        fontScale=1
+        ObjectsList = []
+        
+        for i, c in reversed(list(enumerate(out_classes))):
+            predicted_class = self.class_names[c]
+            box = out_boxes[i]
+            score = out_scores[i]
+
+            label = '{} {:.2f}'.format(predicted_class, score)
+            #label = '{}'.format(predicted_class)
+            scores = '{:.2f}'.format(score)
+
+            top, left, bottom, right = box
+            top = max(0, np.floor(top + 0.5).astype('int32'))
+            left = max(0, np.floor(left + 0.5).astype('int32'))
+            bottom = min(image.shape[0], np.floor(bottom + 0.5).astype('int32'))
+            right = min(image.shape[1], np.floor(right + 0.5).astype('int32'))
+
+            mid_h = (bottom-top)/2+top
+            mid_v = (right-left)/2+left
+
+            # put object rectangle
+            cv2.rectangle(image, (left, top), (right, bottom), self.colors[c], thickness)
+
+            # get text size
+            (test_width, text_height), baseline = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, thickness/self.text_size, 1)
+
+            # put text rectangle
+            cv2.rectangle(image, (left, top), (left + test_width, top - text_height - baseline), self.colors[c], thickness=cv2.FILLED)
+
+            # put text above rectangle
+            cv2.putText(image, label, (left, top-2), cv2.FONT_HERSHEY_SIMPLEX, thickness/self.text_size, (0, 0, 0), 1)
+
+            # add everything to list
+            ObjectsList.append([top, left, bottom, right, mid_v, mid_h, label, scores])
+
+        return image, ObjectsList
+
+    def close_session(self):
+        self.sess.close()
+
+    def detect_img(self, image):
+        image = cv2.imread(image, cv2.IMREAD_COLOR)
+        original_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        original_image_color = cv2.cvtColor(original_image, cv2.COLOR_BGR2RGB)
+        
+        r_image, ObjectsList = self.detect_image(original_image_color)
+        return r_image, ObjectsList
+
+
+def GRABMSS_screen(p_input):
+    while True:
+        #Grab screen image
+        img = np.array(sct.grab(monitor))
+
+        # Put image from pipe
+        p_input.send(img)
+        
+def SHOWMSS_screen(p_output):
+    global fps, start_time
+    yolo = YOLO()
+    while True:
+        img = p_output.recv()
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+        r_image, ObjectsList = yolo.detect_image(img)
+
+        cv2.imshow("YOLO v3", r_image)
+        fps+=1
+        TIME = time.time() - start_time
+        if (TIME) >= display_time :
+            print("FPS: ", fps / (TIME))
+            fps = 0
+            start_time = time.time()
+        if cv2.waitKey(1) & 0xFF == ord('q'): break
+
+    yolo.close_session()
+
+        
+if __name__=="__main__":
+    p_output, p_input = Pipe()
+
+    # creating new processes
+    p1 = multiprocessing.Process(target=GRABMSS_screen, args=(p_input,))
+    p2 = multiprocessing.Process(target=SHOWMSS_screen, args=(p_output,))
+
+    # starting our processes
+    p1.start()
+    p2.start()
