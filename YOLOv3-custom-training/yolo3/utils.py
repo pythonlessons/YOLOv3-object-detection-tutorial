f5510fb66ca64033bf18e535707709dd615789d7
pythonlessons <43852392+pythonlessons@users.noreply.github.com>
2019-08-25 22:45:33 +0300

    Add files via upload

--- YOLOv3-custom-training/yolo3/utils.py
+++ YOLOv3-custom-training/yolo3/utils.py
@@ -17,23 +17,7 @@ def compose(*funcs):
         return reduce(lambda f, g: lambda *a, **kw: g(f(*a, **kw)), funcs)
     else:
         raise ValueError('Composition of empty sequence not supported.')
-'''
-def letterbox_image(image, size):
-    # resize image with unchanged aspect ratio using padding
-    iw, ih = image.size
-    #ih, iw, _ = image.shape
-    #print(iw, ih)
-    w, h = size
-    scale = min(w/iw, h/ih)
-    nw = int(iw*scale)
-    nh = int(ih*scale)
 
-    image = image.resize((nw,nh), Image.BICUBIC)
-    #image = cv2.resize(image, (nh, nw), interpolation=cv2.INTER_CUBIC)
-    new_image = Image.new('RGB', size, (128,128,128))
-    new_image.paste(image, ((w-nw)//2, (h-nh)//2))
-    return new_image
-'''
 def image_preporcess(image, target_size, gt_boxes=None):
 
     ih, iw    = target_size
